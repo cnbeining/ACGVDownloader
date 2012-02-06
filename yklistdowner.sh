f59627e13a90f77895282fe5860d95924a92475d
Lichi Zhang <tigerdavidxeon>
2012-02-06 21:08:56 +0000

    fix bug in yklistdowner.sh

--- yklistdowner.sh
+++ yklistdowner.sh
@@ -18,10 +18,10 @@
 #      REVISION:  ---
 #===============================================================================
 
-pid=$(echo $1 | sed "s/.*id_\([0-9]*\).html/\1/")
+pid=$(echo $1 | sed "s/.*id_\([0-9a-z]*\).html/\1/")
 mkdir $pid;cd $pid; # create a temp folder to download the videos
 curl -o $pid.html $1
-cat $pid".html" | grep 'v_playlist' | sed "s/.*href=\"\(.*\).html.*/\1/" > temp.down
+cat $pid".html" | grep "v_playlist\|\/v_show\/id" | sed "s/.*href=\"\(.*\).html.*/\1/" > temp.down
 uniq temp.down > $pid.down
 rm temp.down
 
