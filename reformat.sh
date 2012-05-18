1b25657646180863001a52d87c86d44ff14bb02d
Lichi Zhang <tigerdavidxeon>
2012-05-18 15:12:44 +0100

    small update

--- reformat.sh
+++ reformat.sh
@@ -7,10 +7,11 @@ for ((i=1;i<=$linenum;i++))
 do
 	input=$(sed -n "$i"'p' < log)
 	filename=$(echo $input | sed 's/\.[^\.]*$//')
-#	extension=$(echo $input | sed 's/^.*\.//')
+	extension=$(echo $input | sed 's/^.*\.//')
 	mv "$input" temp.mp4
-	ffmpeg -i temp.mp4 -threads 2 -acodec libfaac -ab 128k -vcodec libx264 -crf 21 -f mp4 "$filename.mp4"
-	rm temp.mp4
+	cp temp.mp4 "$filename"_backup.$extension
+	ffmpeg -i temp.mp4 -threads 0 -acodec libfaac -ab 128k -vcodec libx264 -crf 21 -f mp4 "$filename.mp4"
+	rm temp.mp4 "$filename"_backup.$extension
 done
 
 rm log
